168c4bf24ed207da495c948d282bf79008e32572
amberGit <wowv58791@163.com>
2015-11-03 12:34:28 +0800

    1st version of zip compression project

---
+++ ZipCompression/ZipEventListener.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace ZipCompression
+{
+    class ZipEventListener: DependencyObject
+    {
+        private ProgressBar PBar = new ProgressBar();
+        private ZipEventSource zipEventIns;
+        private MainWindow window;
+        private TextBlock textBlock = new TextBlock();
+        public static readonly DependencyProperty progressProperty = DependencyProperty.Register("progress", typeof(int), typeof(ZipEventListener));
+        public ZipEventListener (MainWindow window, ZipEventSource zipEventIns)
+        {
+            this.zipEventIns = zipEventIns;
+            this.window = window;
+        }
+
+        public int progress
+        {
+            get
+            {
+                return (int)window.Dispatcher.Invoke(
+                    DispatcherPriority.Background,
+                    (DispatcherOperationCallback)delegate { return getProgressPercent(progressProperty); },
+                    progressProperty);
+            }
+            protected set
+            {
+                window.Dispatcher.BeginInvoke(DispatcherPriority.Background, 
+                    (SendOrPostCallback) delegate { updatePrgressBar(progressProperty, value); }, value);
+            }
+        }
+
+        /// <summary>
+        /// 监听事件
+        /// </summary>
+        public void On()
+        {
+            zipEventIns.ZipEvent += handlePrgressBar;
+        }
+        /// <summary>
+        /// 释放监听事件
+        /// </summary>
+        public void Off()
+        {
+            zipEventIns.ZipEvent -= handlePrgressBar;
+        }
+
+        /// <summary>
+        /// 创建进度条
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void  handlePrgressBar(object sender, EventArgs e)
+        {
+            ZipEventSource.ZipEventArgs eventArgs = e as ZipEventSource.ZipEventArgs;
+            
+            if (progress == 100)
+            {
+                Off();
+                window.SBar.Items.Remove(PBar);
+                textBlock.Text = "压缩完成";
+                window.SBar.Items.Add(textBlock);
+            }
+            else
+            {
+                window.SBar.Items.Remove(textBlock);
+            }
+            if (eventArgs.EventValue == ZipEventSource.ZipEventValue.COMPRESS)
+            {
+                createPrgressBar();
+            }
+           else if (eventArgs.EventValue == ZipEventSource.ZipEventValue.UPDATE_BAR)
+            {
+                ZipUtil zipUtil = sender as ZipUtil;
+                progress = (int) Math.Ceiling(zipUtil.compressedSize * 1.0d / zipUtil.totalSize * 100);
+            }
+        }
+
+        private void createPrgressBar()
+        {
+            PBar.IsIndeterminate = false;
+            PBar.Maximum = 100;
+            PBar.Value =  progress;
+            PBar.Orientation = Orientation.Horizontal;
+            PBar.Width = 200;
+            PBar.Height = 15;
+            PBar.SetBinding(progressProperty, "progress");
+            window.SBar.Items.Add(PBar);
+        }
+
+
+        private void updatePrgressBar(DependencyProperty dp, double value)
+        {
+            //PBar.SetValue(dp, (int)value);
+            PBar.Value = value;
+        }
+        private int getProgressPercent(DependencyProperty dp)
+        {
+            return (int) PBar.Value;
+        }
+    }
+}
---
+++ ZipCompression/ZipEventSource.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipCompression
+{
+    /// <summary>
+    /// 自定义事件类
+    /// </summary>
+    public class ZipEventSource
+    {
+
+        public enum ZipEventValue
+        {
+            COMPRESS,
+            DECOMPRESS,
+            UPDATE_BAR
+        }
+        /// <summary>
+        /// 定义事件参数
+        /// </summary>
+        public class ZipEventArgs : EventArgs
+        {
+            public readonly ZipEventValue EventValue;
+            public ZipEventArgs(ZipEventValue eventValue)
+            {
+                EventValue = eventValue;
+            }
+        }
+
+        /// <summary>
+        /// 声明事件对象
+        /// </summary>
+        public event ZipEventHandler ZipEvent;
+
+        /// <summary>
+        /// 事件触发方法
+        /// </summary>
+        /// <param name="e"></param>
+        public virtual void OnZipEvent(ZipEventArgs e)
+        {
+            if (ZipEvent != null)
+                ZipEvent(this, e);
+        }
+
+        /// <summary>
+        /// 引发事件
+        /// </summary>
+        /// <param name="eventValue"></param>
+        public void RaiseEvent(ZipEventValue eventValue)
+        {
+            ZipEventArgs e = new ZipEventArgs(eventValue);
+            OnZipEvent(e);
+        }
+
+        /// <summary>
+        /// 定义事件处理的委托
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public delegate void ZipEventHandler(object sender, ZipEventArgs e);
+
+    }
+}
